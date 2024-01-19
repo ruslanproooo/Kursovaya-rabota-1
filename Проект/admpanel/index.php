db0940facf9a7c0f7f343633b29ee4e5b79cfeab
K303-13 <K303-13-0@gmail.com>
2024-01-19 12:00:53 +0300

    Инициализация

---
+++ Проект/admpanel/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<header>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/Style/Style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="\Sources\logo.jpg">
+    <title>Pets</title>
+</header>
+<body>
+    <img src="\Sources\logo.jpg">
+    <nav>
+        <a href="/ФамилияГруппа/index.html">Главная</a>
+        <a href="/ФамилияГруппа/admpanel/index.php">Административная панель</a>
+    <a href="/index.php">переходим</a> на вторую страницу
+    </nav>
+    </div>
+</body>
+</html
